07e324d79c4296850011c6d697c391a5a344e2e5
Austin Wood <austin@austinkwood.com>
2017-02-16 16:54:30 -0800

    Display bands at bands/index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resource :session, only: [:create, :destroy, :new, :show]
-  resources :users, only: [:create, :new,]
+  resources :users, only: [:create, :new]
+  resources :bands, only: [:index, :create, :new]
 
   root to: redirect("/session/new")
 end
