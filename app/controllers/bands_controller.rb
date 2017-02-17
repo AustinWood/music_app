a00a70a961ff0f0d72ef3478e9a2128c647f2fea
Austin Wood <austin@austinkwood.com>
2017-02-16 17:13:57 -0800

    Click band to view band page

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -1,6 +1,17 @@
-class BandsController <ApplicationController
+class BandsController < ApplicationController
   def index
     @bands = Band.all
     render :index
+  end
+
+  def show
+    @band = Band.find(params[:id])
+    render :show
+  end
+
+  private
+
+  def band_params
+    params.require(:band).permit(:name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resource :session, only: [:create, :destroy, :new, :show]
   resources :users, only: [:create, :new]
-  resources :bands, only: [:index, :create, :new]
+  resources :bands, only: [:index, :create, :new, :show]
 
   root to: redirect("/session/new")
 end
